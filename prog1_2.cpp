b932c7221afc7f79a9962da8a351b6984ba4ecdc
catb406 <catb406@gmail.com>
2021-03-18 13:01:31 +0300

    Initial commit

---
+++ prog1_2.cpp
@@ -0,0 +1,17 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <cstring>
+#include <wait.h>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[]){
+    printf("prog1 started\nparent pid: %i\nchild pid: %i\n", getppid(), getpid());
+
+    for (int i=0; i<3; i++)
+    {
+        printf("%s\t%s\t%s\n", argv[0], argv[1], argv[2]);
+        sleep(1);
+    }
+    exit(1);
+}
